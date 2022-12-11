efcd6155b9c056e6a006159860c701dc8d8f3cb7
VitalyDabryan <vital.1977@tut.by>
2022-12-12 00:31:19 +0300

    Therd commit

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,4 +2,11 @@ public class Student {
     String fullName; String universityId;
     int currentCourseNumber;
     float avgExamScore;
+
+    public Student(String fullName, String universityId, int currentCourseNumber, float avgExamScore) {
+        this.fullName = fullName;
+        this.universityId = universityId;
+        this.currentCourseNumber = currentCourseNumber;
+        this.avgExamScore = avgExamScore;
+    }
 }
--- src/main/java/University.java
+++ src/main/java/University.java
@@ -3,4 +3,12 @@ public class University {
     String fullName;
     String shortName; int yearOfFoundation;
     StudyProfile mainProfile;
+
+    public University(String id, String fullName, String shortName, int yearOfFoundation, StudyProfile mainProfile) {
+        this.id = id;
+        this.fullName = fullName;
+        this.shortName = shortName;
+        this.yearOfFoundation = yearOfFoundation;
+        this.mainProfile = mainProfile;
+    }
 }
