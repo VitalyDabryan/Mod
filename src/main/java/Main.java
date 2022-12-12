af9063b4d9d7ad936cb606425f8414b9664c0dcf
VitalyDabryan <vital.1977@tut.by>
2022-12-12 21:57:45 +0300

    4 commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println(StudyProfile.MEDICINE.getProfileName());
+
+        University university1 = new University("1", "Belorussian State University", "BSU", 1929, StudyProfile.PHILOSOPHY);
+        University university2 = new University("2", "Belorussian State University of Transport", "BelSUT", 1953, StudyProfile.TELECOMMUNICATIONS);
+
+        Student student1 = new Student("Vitali Dabryian", "2", 1,99);
+        Student student2 = new Student("Anton Ivanich", "1", 2,95);
+
+        System.out.println(university2);
+        System.out.println(student1);
     }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -9,4 +9,46 @@ public class Student {
         this.currentCourseNumber = currentCourseNumber;
         this.avgExamScore = avgExamScore;
     }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getUniversityId() {
+        return universityId;
+    }
+
+    public int getCurrentCourseNumber() {
+        return currentCourseNumber;
+    }
+
+    public float getAvgExamScore() {
+        return avgExamScore;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setUniversityId(String universityId) {
+        this.universityId = universityId;
+    }
+
+    public void setCurrentCourseNumber(int currentCourseNumber) {
+        this.currentCourseNumber = currentCourseNumber;
+    }
+
+    public void setAvgExamScore(float avgExamScore) {
+        this.avgExamScore = avgExamScore;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "fullName='" + fullName + '\'' +
+                ", universityId='" + universityId + '\'' +
+                ", currentCourseNumber=" + currentCourseNumber +
+                ", avgExamScore=" + avgExamScore +
+                '}';
+    }
 }
